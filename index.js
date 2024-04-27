e7761b466069da0516bcbaa86c53f735061aa84e
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-27 12:12:58 +0600

    created post and get routes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fabric-fusion-assignment-ten.web.app/",
+    ],
+  })
+);
 app.use(express.json());
 
 // Connecting to MongoDB
@@ -26,6 +33,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const craftCollection = client.db("craftDB").collection("craft");
+
+    app.get("/crafts", async (req, res) => {
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/crafts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const craft = await craftCollection.findOne(query);
+      res.send(craft);
+    });
+
+    app.post("/crafts", async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
